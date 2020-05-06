ee18c81e4e1ddc1a52f567f0124fe9a715501ace
Ivelin Ivanov <ivelin@ambianic.ai>
2020-05-06 11:23:00 -0500

    feat: add service worker pwa js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const version = "5.5.2020";
+const cacheName = `wwwambianicai-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
